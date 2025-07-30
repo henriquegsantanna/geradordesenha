32d76af5220f8499bc0170bbc7b557ddb880f0f0
Henrique Gaiardoni <167096110+henriquegsantanna@users.noreply.github.com>
2025-07-30 16:09:38 -0300

    Correcao de codigo

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 def gerarSenha():
-    print("1 - Gerar senha pouco segura")
-    print("2 - Gerar senha segura")
-    print("3 - Gerar senha muito segura")
-    x = input("Selecione qual senha quer gerar: ")
-    
-    if x is not int:
-        print("Opcao invalida. Digite somente o numero: ")
-        return
-    elif x == "1":
-        print("Opcao 1 selecionada.")
-    elif x == "1":
-        print("Opcao 2 selecionada.")
-    elif x == "1":
-        print("Opcao 3 selecionada.")
-    else:
-        print("Opcao invalida.")
-        return
+    while True:
+        print("1 - Gerar senha pouco segura")
+        print("2 - Gerar senha segura")
+        print("3 - Gerar senha muito segura")
+        x = input("Selecione qual senha quer gerar: ")
+        
+        if x == "1":
+            print("Opcao 1 selecionada.")
+            break
+        elif x == "2":
+            print("Opcao 2 selecionada.")
+            break
+        elif x == "3":
+            print("Opcao 3 selecionada.")
+            break
+        else:
+            print("Opcao invalida.")
+            return
 
 while True:
-    print("-"*5 + "GERADOR DE SENHA" + "-"*5)
+    print("-"*7 + "GERADOR DE SENHA" + "-"*7)
+    gerarSenha()
